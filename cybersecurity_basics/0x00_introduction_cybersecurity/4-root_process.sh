b198d2ebae73994fbe162ece9a0e4aa8b4a17a31
Solomon William <solomon@kali.solo>
2024-07-30 23:38:14 +0100

    corrected task 4 x3

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-USER="$1"
-ps aux | awk -v user="$USER"  '$1 == user { if ($5 > 0) { if ($6 > 0) print } }'
+ps aux | awk -v user="$1"  '$1 == user { if ($5 > 0) { if ($6 > 0) print } }'
