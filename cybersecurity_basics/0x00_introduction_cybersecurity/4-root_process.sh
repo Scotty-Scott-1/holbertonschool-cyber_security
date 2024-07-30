9a91a88007d7dae242ced055feade24d10328dad
Solomon William <solomon@kali.solo>
2024-07-30 23:41:12 +0100

    corrected task 4 x4

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps aux | awk -v user="$1"  '$1 == user { if ($5 > 0) { if ($6 > 0) print } }'
+ps aux | awk -v user="$1"  '$1 == user { if ($5 > 0) { if ($6 > 0) print } }' | grep -v "grep"
