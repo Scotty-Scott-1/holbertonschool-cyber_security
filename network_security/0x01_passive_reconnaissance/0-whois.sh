cd85ff3c6f0808bfa7d9128f14cc4725fb72cb36
Solomon William <solomon@kali.solo>
2024-08-05 23:17:30 +0100

    corrected task 0: correct output format redirected to a file

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk '!/Registry Registrant ID/ && /Registrant/ {gsub(/:/, ",", $0); print $0} /Admin/ {gsub(/:/, ",", $0); print $0} /Tech/ {gsub(/:/, ",", $0); print $0}'
+whois "$1" | awk '!/Registry Registrant ID/ && /Registrant/ {gsub(/:/, ",", $0); print $0} /Admin/ {gsub(/:/, ",", $0); print $0} /Tech/ {gsub(/:/, ",", $0); print $0}' > "$1".csv
