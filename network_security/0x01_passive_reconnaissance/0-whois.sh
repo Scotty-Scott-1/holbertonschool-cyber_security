63f9d274ec4082453b631dcc6d1def75d15f6362
Solomon William <solomon@kali.solo>
2024-08-08 14:44:58 +0100

    corrected task 0: trying no  around args

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk '!/Registry Registrant ID/ && /Registrant/ {gsub(/:/, ",", $0); print $0} !/Registry Admin ID/ && /Admin/ {gsub(/:/, ",", $0); print $0} !/Registry Tech ID/ && /Tech/ {gsub(/:/, ",", $0); print $0}' > $1.csv && cat $1.csv
+whois $1 | awk '!/Registry Registrant ID/ && /Registrant/ {gsub(/:/, ",", $0); print $0} !/Registry Admin ID/ && /Admin/ {gsub(/:/, ",", $0); print $0} !/Registry Tech ID/ && /Tech/ {gsub(/:/, ",", $0); print $0}' > $1.csv && cat $1.csv
