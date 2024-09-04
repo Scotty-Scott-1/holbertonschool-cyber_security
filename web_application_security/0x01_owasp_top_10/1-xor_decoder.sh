043b764246cb9970ba946ee4c2a34c5b88912103
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-09-04 05:37:48 -0400

    added task 1

---
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sliced_password=${1#"{xor}"};
+decoded_base64=$(echo "$sliced_password" | base64 --decode);
+decoded_password=""
+
+for ((i=0; i<${#decoded_base64}; i++)); do  
+	char=${decoded_base64:i:1}
+	ascii_value=$(printf "%d" "'$char'")
+	xor_value=$((ascii_value ^ 95))
+	xor_char=$(printf "\\$(printf '%03o' "$xor_value")")
+	decoded_password+="$xor_char"
+done
+echo "$decoded_password"
+
