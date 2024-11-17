70c895e19cd00b7f556902f809021d8ee37fa717
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-11-17 10:57:18 +0100

    updated 0

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -4,6 +4,16 @@ replace a string in pid
 """
 import sys
 
+
+def usage_error():
+    """desplay usage error message"""
+    print("Usage: ./read_write_heap.py pid search_string replace_string")
+    sys.exit(1)
+
+
+if len(sys.argv) < 4:
+    usage_error()
+
 pid = sys.argv[1]
 target_str = sys.argv[2]
 new_str = sys.argv[3]
@@ -15,13 +25,9 @@ try:
                 address_range = line.split()[0]
                 heap_start, heap_end = [int(adr, 16) for adr in address_range
                                         .split("-")]
-except Exception as e:
-    print("error: {}.".format(e))
-    sys.exit(1)
 
-mem_file_path = f"/proc/{pid}/mem"
+    mem_file_path = f"/proc/{pid}/mem"
 
-try:
     with open(mem_file_path, "r+b") as mem_file:
         mem_file.seek(heap_start)
         heap_data = mem_file.read(heap_end - heap_start)
@@ -33,6 +39,13 @@ try:
         mem_file.seek(target_address)
         mem_file.write(replacement)
 
+
+except PermissionError:
+    print("Try running sudo.")
+    sys.exit(1)
+except FileNotFoundError:
+    print(f"{pid} not found.")
+    sys.exit(1)
 except Exception as e:
-    print("error: {}.".format(e))
+    print(f"Error: {e}")
     sys.exit(1)
