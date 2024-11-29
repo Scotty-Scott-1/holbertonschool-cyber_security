bca99c70364daa13c9518ce597a3a3f57ce40f02
Solomon William <kali@kali.kali>
2024-11-29 11:23:53 +0100

    added task 5 x3

--- web_application_security/0x0c_web_application_foresics/5-users.sh
+++ web_application_security/0x0c_web_application_foresics/5-users.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-str=$(grep "new user: name=" auth.log|awk '{ print substr($8, 6) }'|sort|awk '{ printf $0 }'); echo "${str::-1}"
+str=$(grep "new user: name=" auth.log|awk '{ print substr($8, 6) }'|sort|awk '{ printf $0 }')
+echo "${str::-1}"
