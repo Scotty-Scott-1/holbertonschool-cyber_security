37a15ee41b849cfeb8538843ebd08afb9ccb813a
Solomon William <kali@kali.kali>
2024-12-12 17:21:30 +0100

    added task 4 x2

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -3,7 +3,7 @@ require 'json'
 def merge_json_files(file1_path, file2_path)
   file_1 = JSON.parse(File.read(file1_path))
   file_2 = JSON.parse(File.read(file2_path))
-  merged_file = file_1 + file_2
+  merged_file = file_2 + file_1
 
   File.open(file2_path, 'w') do |file|
     file.write(JSON.pretty_generate(merged_file))
