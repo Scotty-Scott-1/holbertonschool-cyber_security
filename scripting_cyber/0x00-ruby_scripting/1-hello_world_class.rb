ae5bc34c55f356fd3773637af2dda46453f823d8
Solomon William <kali@kali.kali>
2024-12-12 13:22:08 +0100

    added task 1

---
+++ scripting_cyber/0x00-ruby_scripting/1-hello_world_class.rb
@@ -0,0 +1,15 @@
+# !/usr/bin/env ruby
+# frozen_string_literal: true
+
+# a HelloWorld class that prints Hello World
+class HelloWorld
+  attr_reader :message
+
+  def initialize
+    @message = 'Hello World!'
+  end
+
+  def print_hello
+    puts message
+  end
+end
