f83caeafad399c5b8a108ec353bfb25fe2d2c223
Solomon William <kali@kali.kali>
2024-12-14 15:42:11 +0100

    added task 8

---
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+
+def post_request(url, body_params)
+
+  my_url = URI.parse(url)
+  headers = {'Content-Type' => 'application/json'}
+  
+  http = Net::HTTP.new(my_url.host, my_url.port)
+  http.use_ssl = (my_url.scheme = 'https')
+  request = Net::HTTP::Post.new(my_url.path, headers)
+  request.body = body_params.to_json
+  response = http.request(request) 
+
+  code = response.code
+  
+
+  puts "Response status: #{response.code} #{response.message}"
+  puts 'Response body:'
+  if code == "404"
+    puts "{\n}"
+    
+  else
+  puts response.body
+  end
+end
