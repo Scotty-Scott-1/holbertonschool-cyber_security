a83553c83d7adb5487d614266556c9da0f2fd96b
Solomon William <kali@kali.kali>
2024-12-14 02:50:31 +0100

    added task 7 x3

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,6 +1,10 @@
 def print_arguments
+  if ARGV.length == 0
+    puts 'No arguments provided.'
+  else
   puts 'Arguments:'
   puts "1. #{ARGV[0]}"
   puts "2. #{ARGV[1]}"
   puts "3. #{ARGV[2]}"
+  end
 end
