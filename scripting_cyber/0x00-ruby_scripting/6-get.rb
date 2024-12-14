53d390110463c57982b32b7d3d899587ad38f460
Solomon William <kali@kali.kali>
2024-12-14 02:30:13 +0100

    added task 6 x2

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -6,8 +6,15 @@ def get_request(url)
 
   my_url = URI.parse(url)
   response = Net::HTTP.get_response(my_url)
+  code = response.code
+
 
   puts "Response status: #{response.code} #{response.message}"
   puts 'Response body:'
+  if code == "404"
+    puts "{\n}"
+    
+  else
   puts response.body
+  end
 end
