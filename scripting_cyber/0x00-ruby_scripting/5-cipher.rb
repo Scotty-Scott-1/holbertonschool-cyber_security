c917a5957ea6786ace34f2e6a99df5fa7c92f35c
Solomon William <kali@kali.kali>
2024-12-14 01:30:45 +0100

    added task 5 x2

--- scripting_cyber/0x00-ruby_scripting/5-cipher.rb
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -55,4 +55,32 @@ class CaesarCipher
 
     return new_string
   end
+
+  def cipher(message, shift)
+    instance_shift = shift
+    caps = ('A'..'Z').to_a
+    lower = ('a'..'z').to_a
+    new_string = ''
+
+    message.chars.each do |char|
+      if char.ord >= 65 && char.ord <= 90
+        new_index_of_letter = caps.index(char) + instance_shift % 26
+        new_letter = caps[new_index_of_letter]
+        new_string += new_letter
+
+      elsif char.ord >= 97 && char.ord <= 122
+        new_index_of_letter = lower.index(char) + instance_shift % 26
+        new_letter = lower[new_index_of_letter]
+        new_string += new_letter
+      else
+        new_string += char
+      end
+    end
+
+    return new_string
+  end
+
+
+
+
 end
